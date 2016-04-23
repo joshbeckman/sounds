d99d09038a4dfa1b3a6b9af36973866b610817fa
andjosh <email@andjosh.com>
2016-04-23 11:05:37 -0500

    Clean up io route syntax

--- routes/io.js
+++ routes/io.js
@@ -1,14 +1,10 @@
+
 var globals = require('../globals');
 
 module.exports = function (app){
-
-// middleware for scoket.io's connect and disconnect
-app.io.use(function* (next) {
-  // on connect
-    console.log('connect');
-  yield* next;
-    console.log('disconnect');
-  // on disconnect
-});
-
+    app.io.use(function* (next) {
+        console.log('connect');
+        yield* next;
+        console.log('disconnect');
+    });
 };
