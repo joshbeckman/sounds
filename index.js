cb4f8977fa1214234091d5eed75d1a5b8df126c7
andjosh <email@andjosh.com>
2016-04-23 11:04:49 -0500

    Change syntax on index script

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var koa       = require('koa.io'),
-    compress  = require('koa-compress'),
-    jade      = require('koa-jade'),
-    route     = require('koa-route'),
-    koaBody   = require('koa-better-body'),
-    path      = require('path'),
+var koa         = require('koa.io'),
+    compress    = require('koa-compress'),
+    jade        = require('koa-jade'),
+    route       = require('koa-route'),
+    koaBody     = require('koa-better-body'),
+    path        = require('path'),
     staticCache = require('koa-static-cache'),
-    globals   = require('./globals'),
-    app       = koa();
+    globals     = require('./globals'),
+    app         = koa(),
+    port        = process.env.PORT || process.env.NODE_PORT || 3001;
 
 app.use(compress());
 app.use(staticCache(path.join(__dirname, 'public')));
@@ -16,13 +17,12 @@ app.use(koaBody({
 }));
 
 app.use(jade.middleware({
-  viewPath: __dirname + '/views',
-  debug: false,
-  pretty: false,
-  compileDebug: false
+    viewPath: __dirname + '/views',
+    debug: false,
+    pretty: false,
+    compileDebug: false
 }));
 
-// logger
 app.use(function *(next){
     var start = new Date;
     yield next;
@@ -33,4 +33,5 @@ app.use(function *(next){
 require('./routes/io')(app);
 require('./routes/url')(app, route);
 
-app.listen(process.env.PORT || process.env.NODE_PORT || 3001);
+app.listen(port);
+console.info('listening on', port);
