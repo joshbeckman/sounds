61427f4862cd1a0fda1090ec79b294e2375c27f7
andjosh <email@andjosh.com>
2016-04-23 11:05:46 -0500

    Use rooms to communicate

--- public/the.js
+++ public/the.js
@@ -1,57 +1,53 @@
-(function(window, document, soundsLike) {
-    var h1 = document.querySelector('h1'),
-        p = document.querySelector('p'),
-        encounters = [293.66, 329.63, 261.63, 130.81, 196];
-
-    window.addEventListener('load', onload, false);
-    window.soundsLike = soundsLike || {};
-
-    function onload() {
-        window.soundsLike.socket = io();
-        window.soundsLike.socket.on(
-            urlParams.id || 'record',
-            record
-        );
-        if (urlParams.id) {
-            h1.innerHTML =
-                "<a href='" + window.location.href + "'>" + urlParams.id + "</a><br>" +
-                h1.innerText;
-            document.title = urlParams.id + ' Sounds';
-        }
-    }
-
-    function record(data) {
-        var i = window.soundsLike.startTone(
-            data.tone ||
-            encounters[
-                Math.floor(Math.random() * encounters.length)
-            ]
-        );
-
-        setTimeout(
-            window.soundsLike.stopTone,
-            data.msec || 500,
-            i
-        );
-        document.body.style.background =
-            'hsl(' + (data.tone || 432) % 360 + ',100%,50%)';
-        if (data.msg)
-            p.textContent = data.msg;
-    }
-})(this, this.document, this.soundsLike);
-
-var urlParams;
-
+window.soundsLike = window.soundsLike || { urlParams: null };
 (window.onpopstate = function() {
     var match,
         pl     = /\+/g,
         search = /([^&=]+)=?([^&]*)/g,
         query  = window.location.search.substring(1),
         decode = function(s) {
-            return decodeURIComponent(s.replace(pl, " "));
+            return decodeURIComponent(s.replace(pl, ' '));
         };
 
-    urlParams = {};
+    window.soundsLike.urlParams = {};
     while (match = search.exec(query))
-       urlParams[decode(match[1])] = decode(match[2]);
+        window.soundsLike.urlParams[decode(match[1])] = decode(match[2]);
 })();
+(function(window, document) {
+    var h1 = document.querySelector('h1'),
+        p = document.querySelector('p'),
+        encounters = [293.66, 329.63, 261.63, 130.81, 196],
+        urlParams = window.soundsLike.urlParams;
+
+    document.querySelector('#socketScript')
+        .addEventListener('load', onload, false);
+
+    function onload() {
+        window.soundsLike.socket = window.io('/' + (urlParams.id || ''));
+        window.soundsLike.socket.on('sound', record);
+        if (urlParams.id) {
+            h1.innerHTML =
+                '<a href="' + window.location.href + '>' +
+                    urlParams.id +
+                '</a><br>' +
+                h1.innerText;
+            document.title = urlParams.id + ' Sounds';
+        }
+    }
+
+    function record(data) {
+        var tone = data.tone || randomEncounter();
+
+        setTimeout(window.soundsLike.stopTone, data.msec || 500,
+            window.soundsLike.startTone(tone)
+        );
+        document.body.style.background =
+            'hsl(' + tone % 360 + ',100%,50%)';
+        if (data.msg) p.textContent = data.msg;
+    }
+
+    function randomEncounter() {
+        return encounters[
+            Math.floor(Math.random() * encounters.length)
+        ];
+    }
+})(this, this.document);
--- routes/url.js
+++ routes/url.js
@@ -1,25 +1,24 @@
+
 var globals = require('../globals');
 
 module.exports = function (app, route){
 
-app.use(route.get('/', function* () {
-    yield this.render('index', {
-        title: 'Sounds',
-        globals: globals
-    });
-}));
+    app.use(route.get('/', function* () {
+        yield this.render('index', {
+            title: 'Sounds',
+            globals: globals
+        });
+    }));
 
-app.use(route.get('/record', function* () {
-    this.status = 102;
+    app.use(route.get('/record', function* () {
+        this.status = 102;
 
-    app.io.sockets.emit(
-        this.query.id || 'record',
-        this.query
-    );
+        app.io('/' + (this.query.id || ''))
+        .emit('sound', this.query);
 
-    this.body = globals.message
-    this.status = 200;
-}));
-
+        this.type = 'image/svg+xml';
+        this.body = globals.message
+        this.status = 200;
+    }));
 
 };
